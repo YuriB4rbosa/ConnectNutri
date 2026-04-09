dc325e27ac6d7aa29df2bbf9fc89b2beab100cbd
Yuri <yuri.damico18@gmail.com>
2026-04-09 13:26:04 -0300

    alteraçoes para o deploy - Procfile

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'chave-provisoria-segura')
 
+
+with app.app_context():
+    criar_tabelas()
+
 configurar_rotas(app)
 
-
 if __name__ == '__main__':
-    with app.app_context():
-        criar_tabelas()
-    
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
